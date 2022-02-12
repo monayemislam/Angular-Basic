e8294ab3207dae55d129548f5815f9d241528260
Monayem Islam <monayemislam@gmail.com>
2022-02-12 23:59:39 +0600

    string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,10 @@ import { Component } from "@angular/core";
     templateUrl: './server.component.html'
 })
 export class ServerComponent{
+    serverId:number = 12;
+    serverStatus:string = 'offline';
 
+    public getServerStatus(){
+       return this.serverStatus;
+    }
 }
